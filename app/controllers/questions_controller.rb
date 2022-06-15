b7e862604e0c03807e7352a81e564360af6e09f0
leandro lima <leandro.desouzalima1@gmail.com>
2022-06-15 07:05:28 -0300

    complete app

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question]
+   if @question.blank?
+    @answer = "I can't hear you!"
+   elsif  @question =~ /i am going to work/i
+    @answer = 'Great!'
+   elsif  @question.ends_with?("?")
+    @answer = "Silly question, get dressed and go to work!"
+   else
+    @answer = "I don't care, get dressed and go to work!"
+   end
+  end
+end
